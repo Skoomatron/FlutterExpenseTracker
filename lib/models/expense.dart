a67dc1acd40f3e7f7cfa67af2b37d3ec4038f631
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2023-05-24 10:45:43 -0400

    Icons And Formatted Date

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,7 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
+
+final formatter = DateFormat.yMd();
 
 const uuid = Uuid();
 enum Category { food, travel, leisure, work }
+
+const categoryIcons = {
+  Category.food: Icons.dining,
+  Category.travel: Icons.airplanemode_active,
+  Category.work: Icons.work,
+  Category.leisure: Icons.movie
+};
 
 class Expense {
   final String id;
@@ -16,4 +27,8 @@ class Expense {
     required this.date,
     required this.category,
   }) : id = uuid.v4();
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
 }
